fcfbecd676918972616a3cdf4fb8fd2da78ad2b0
chuhsuanchen <140677606+chuhsuanchen@users.noreply.github.com>
2024-09-06 09:11:28 +1000

    Adds module

--- src/screens/ProductDetailScreen.js
+++ src/screens/ProductDetailScreen.js
@@ -1,9 +1,36 @@
+import styles from "./ProductDetailScreen.module.css";
 
 const ProductDetailsScreen = () => {
   return (
-    <div>
+    <div className={styles.ProductDetailScreen}>
+      <div className={styles.ProductDetailScreenTitle}>
+        <p>Product Detail</p>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.dataContainerHolder}>
+          <div className={styles.productDetailDescriptionContainer}>
+          <img className={styles.pot} src="/assets/images/3d design.jpg" alt="Product Motor" />
+          </div>
+          <div className={styles.productDetailDescription}>
+            <p>
+            This flowerpot design combines mechanical and electronic components to create an interactive, posture-sensitive flowerpot that reacts by blooming or closing its petals, potentially offering a unique reward or alert system.            
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.dataContainerHolder}>
+          <div className={styles.productDetailDescriptionContainer}>
+            <img className={styles.pot} src="/assets/images/3d design.jpg" alt="Product Motor" />
+          </div>
+          <div className={styles.productDetailDescription}>
+            <p>
+              The motor mechanism plays a crucial role in controlling the vibrations and adjusting the plant's growth pattern according to user interactions. It adds a dynamic element to the project, making it both engaging and functional.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetailsScreen;
