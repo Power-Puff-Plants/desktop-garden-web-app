3db71a818deebd208aa6d0f98ea06f91f5ed7fc0
chuhsuanchen <140677606+chuhsuanchen@users.noreply.github.com>
2024-09-13 10:39:09 +1000

    Adds hamburger menu finalised

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import styles from "./NavigationBar.module.css";
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false); // State to handle menu open/close
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.NavigationBar}>
-      <img className={styles.pppLogo} src="/assets/images/ppp-logo.png"/>
-      <div className={styles.rightNavContainer}>
+      <img className={styles.pppLogo} src="/assets/images/ppp-logo.png" />
+      <div className={`${styles.rightNavContainer} ${isOpen ? styles.showMenu : ""}`}>
         <div className={styles.routesContainer}>
           <Link to='/' className={styles.link}>Home</Link>
           <Link to='/product-detail' className={styles.link}>Product Detail</Link>
           <Link to='/data' className={styles.link}>Analysis</Link>
           <Link to='/login' className={styles.link}>Log in</Link>
         </div>
-        <img className={styles.profileImg} src="/assets/images/profile-img.png"/>
+        <img className={styles.profileImg} src="/assets/images/profile-img.png" />
       </div>
-     </div>
+      <button className={styles.hamburger} onClick={toggleMenu}>
+        ☰
+      </button>
+    </div>
   );
 };
 
